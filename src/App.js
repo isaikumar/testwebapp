dc789254e0c3f0512838a8420649ec60a8aa9fdf
isaikumar <isaikumarreddy@gmail.com>
2022-01-31 07:17:26 +0530

    configured routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./Login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      my app
+    <Router>
+      <Routes>
+        {/* Default Route when the page is loaded */}
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </Router>
     </div>
   );
 }
