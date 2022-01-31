9a04b31e914205c8383a11e69b9ce20e076ecb6e
isaikumar <isaikumarreddy@gmail.com>
2022-01-31 07:57:02 +0530

    integrated formik validations

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./style.css";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const Login = () => {
+  const initialValues = { emailId: "", Password: "", rememberMe: false };
+  const validationSchema = yup.object({
+    emailId: yup
+      .string()
+      .email("Enter A valid Email Id")
+      .required("Email Id is Required"),
+    Password: yup.string().required("Password is Required"),
+    rememberMe: yup.boolean(),
+  });
 
+  const onSubmit = (values) => {
+
+  }
+  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+//Formik Intilization by passing intialvalues and validation and submitHandling
   return (
     <div className="container">
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div className="card">
           <h3 className="card-header">Sign in</h3>
           <div>
@@ -16,7 +32,13 @@ const Login = () => {
                   type="text"
                   name="emailId"
                   placeholder="Email"
+                  {...formik.getFieldProps("emailId")}
                 ></input>
+                {formik.touched.emailId && formik.errors.emailId ? (
+                  <span className="error">Email Id is Required</span>
+                ) : (
+                  ""
+                )}
                 <>
                 </>
               </div>
@@ -26,7 +48,13 @@ const Login = () => {
                   name="Password"
                   placeholder="Password"
                   type="password"
+                  {...formik.getFieldProps("Password")}
                 ></input>
+                {formik.touched.Password && formik.errors.Password ? (
+                  <span className="error">Password is Required</span>
+                ) : (
+                  ""
+                )}
                 <>
                 </>
               </div>
@@ -37,13 +65,14 @@ const Login = () => {
                     <input
                       type="checkbox"
                       name="rememberMe"
+                      {...formik.getFieldProps("rememberMe")}
                     />{" "}
                     Remember me?{" "}
                   </label>
                 </div>
               </div>
               <div>
-                <button>Sign in</button>
+                <button type="button">Sign in</button>
               </div>
               <div className="additional-links-container">
                 <a href="#">Forgot Your Password?</a>
