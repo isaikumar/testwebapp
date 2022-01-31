145e2305ef27f2e8ea9be63f19ff4882ea37ef74
isaikumar <isaikumarreddy@gmail.com>
2022-01-31 07:38:20 +0530

    form layout design using pure css

--- src/Login/index.js
+++ src/Login/index.js
@@ -5,7 +5,57 @@ const Login = () => {
 
   return (
     <div className="container">
-      Login
+      <form>
+        <div className="card">
+          <h3 className="card-header">Sign in</h3>
+          <div>
+            <div>
+              <div>
+                <label>Email</label>
+                <input
+                  type="text"
+                  name="emailId"
+                  placeholder="Email"
+                ></input>
+                <>
+                </>
+              </div>
+              <div>
+                <label>Password</label>
+                <input
+                  name="Password"
+                  placeholder="Password"
+                  type="password"
+                ></input>
+                <>
+                </>
+              </div>
+              <div>
+                <div className="checkbox">
+                  <label>
+                    {" "}
+                    <input
+                      type="checkbox"
+                      name="rememberMe"
+                    />{" "}
+                    Remember me?{" "}
+                  </label>
+                </div>
+              </div>
+              <div>
+                <button>Sign in</button>
+              </div>
+              <div className="additional-links-container">
+                <a href="#">Forgot Your Password?</a>
+                <span>
+                  Don't have an account? <a href="#">Sign up</a>
+                </span>
+                <a href="#">Resend email confirmation</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
